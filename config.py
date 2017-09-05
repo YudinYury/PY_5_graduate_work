7e9409d5e139f42a5fda3e85ba8acb91c537a8d5
YudinYury <yudin.yury@gmail.com>
2017-09-05 10:15:12 +0300

    graduate work

---
+++ config.py
@@ -0,0 +1,23 @@
+vk_access_token = "333f1570460ac01a118e6a5a61e5948dff7deb5d9c5311f81f4e2fd1f9094739c601388618a4ff837ed19"
+
+vk_login = '79100889800'
+vk_password = '33129337'
+
+vk_json = {
+    "app_name": "test_API",
+    "app_id": "6111947",
+    "client_id": "6111947",
+    "secure_key": "dZJyhs1xOcj7NHva31kR",
+    "service_key": "b4f48efeb4f48efeb4f48efea5b4a9cc35bb4f4b4f48efeed93a9520c9f72d06c6374e9",
+    "redirect_url": "https://oauth.vk.com/blank.html",
+    "display": "",
+    "scope": "notify,friends,status,notes,groups,wall,messages,offline",
+    "response_type": "token",
+    "version": "5.68",
+    "description": {
+        "app_name": "наименование приложения",
+        "app_id": "ID приложения",
+        "secure_key": "защищенный ключ",
+        "service_key": "Сервисный ключ доступа"
+        }
+    }
