175df3682de0495b5f9e98c684395835094a46b8
YudinYury <yudin.yury@gmail.com>
2017-09-06 12:10:52 +0300

    Fix issue #1

--- PY_5_graduate_work.py
+++ PY_5_graduate_work.py
@@ -56,9 +56,10 @@ from time import sleep
 import requests
 
 from config import VK_ACCESS_TOKEN
-from config import root_vk_id
+from config import root_vk_id  #  root_vk_id = '5030613'
 
-logging.basicConfig(level=logging.ERROR, format='[LINE:%(lineno)d]# %(asctime)s - %(levelname)s - %(message)s')
+# levels (from low to high): DEBUG, INFO, WARNING, ERROR, CRITICAL.
+logging.basicConfig(level=logging.INFO, format='[LINE:%(lineno)d]# %(asctime)s - %(levelname)s - %(message)s')
 # logging.basicConfig(level=logging.DEBUG, format=u'[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s')
 # logging.basicConfig(format=u'%(levelname)-8s [%(asctime)s] %(message)s', level=logging.DEBUG,
 #                     filename=u'graduate_work.log')
@@ -71,9 +72,9 @@ class VkFriends():
     root_friend_id = None  # root_vk_id = '5030613'
     root_friend_first_name = None
     root_friend_last_name = None
-    root_friend_count = 0
-    root_friend_id_list = []
-    root_friend_id_set = None
+    root_friends_count = 0
+    root_friends_id_list = []
+    root_friends_id_set = None
     groups_count = 0
     root_friend_groups_set = None
     different_group_set = None
@@ -102,20 +103,24 @@ class VkFriends():
         params = self.make_vk_params()
         execution_code, response_json = self._do_vk_request('users.get', params)
         response_json = response_json['response'][0]
-
-        if not response_json:
-            print('Root friend ID is incorrect. Bye ...')
-            exit(0)
-
-        # response_json = response.json()['response'][0]
         self.root_friend_first_name = response_json['first_name']
         self.root_friend_last_name = response_json['last_name']
-        print('Root friend {} is founded.'.format(self.root_friend_id))
+        logging.info('__init__: execution_code -> {}'.format(execution_code))
+        logging.info('response_json -> {}'.format(response_json))
+
+        if not response_json :
+            print('Root friend ID is incorrect. Bye ...')
+            exit(0)
+        elif response_json['first_name'] == 'DELETED':
+            print('Root friend is DELETED. Bye ...')
+            exit(0)
+        else:
+            print('Root friend {} is founded.'.format(self.root_friend_id))
 
     def print_root_user_info(self):
         print('--------- info about root friend ---------')
         print('Name: {} {}'.format(self.root_friend_first_name, self.root_friend_last_name))
-        print('friend_count = {}'.format(self.root_friend_count))
+        print('friend_count = {}'.format(self.root_friends_count))
         # print('self.friend_id_list =', self.friend_id_set)
         print('groups_count = {}'.format(self.groups_count))
 
@@ -189,15 +194,11 @@ class VkFriends():
         # print(self.vk_group_result_list)
 
         print('Saving report to file.')
-        with open('groups.json', 'w') as f:  # , encoding='utf-8'
-            json.dump(self.vk_group_result_list, f)
-            # json.dump(self.vk_group_result_list, f, ensure_ascii=False)
+        with open('groups.json', 'w', encoding='utf-8') as f:  # , encoding='utf-8'
+            json.dump(self.vk_group_result_list, f, ensure_ascii=False)
 
-        with open('groups.json.txt', 'w') as f:
-            # json.dump(self.vk_group_result_list, f, ensure_ascii=False)
-            json.dump(self.vk_group_result_list, f)
-        # with open('groups.json.asci.txt', 'w') as f:
-        #     json.dump(self.vk_group_result_list, f)
+        with open('groups.json.txt', 'w', encoding='utf-8') as f:
+            json.dump(self.vk_group_result_list, f, ensure_ascii=False)
         print('Have done.')
 
     def _do_vk_users_get_request(self, params):
@@ -215,33 +216,11 @@ class VkFriends():
         else:
             return response_json
 
-    def _do_vk_friends_get_request(self, params):
-        execution_code, response_json = self._do_vk_request('friends.get', params)
-        return execution_code, response_json['response']
-
-    def make_root_friend_id_list(self):
-        """
-        https://api.vk.com/method/users.get?
-        user_id=<user_id> User, for which a list of friends is created
-         & access_token=<VK-token>
-         & v=<версия интерфейса VK>
-         & count=<количество выводимых id друзей>
-        :param self: <int> or <str> self.root_friend
-        :return: <int> friend_count and <list> friend_id_list
-        """
-        params = self.make_vk_params()
-        if SHORT_RUN:
-            params['count'] = 117
-        execution_code, response_json = self._do_vk_friends_get_request(params=params)
-        self.root_friend_count = response_json['count']
-        self.root_friend_id_list = response_json['items']
-        self.root_friend_id_set = set(self.root_friend_id_list)
-
     def root_friend_make_groups_set(self):
         params = self.make_vk_params()
         groups_count, root_friend_groups_set = self._any_make_groups_set(self.root_friend_id)
         if groups_count == 0:
-            logging.info('root_friend_make_groups_set() not done')
+            logging.debug('root_friend_make_groups_set() not done')
             return 0
 
         self.groups_count = groups_count
@@ -280,12 +259,11 @@ class VkFriends():
                                                                    response_json['error']['error_msg']))
             return 0, response_json['error']
 
-
     def make_different_group_list(self):
         counter = 0
-        for friend_id in self.root_friend_id_set:
+        for friend_id in self.root_friends_id_set:
             if counter % 10 == 0:
-                print('Find exclusive groups: {} from {}'.format(counter, len(self.root_friend_id_set)))
+                print('Find exclusive groups: {} from {}'.format(counter, len(self.root_friends_id_set)))
             friend_groups_set_num, friend_groups_set = self._any_make_groups_set(vk_id=friend_id)
             counter += 1
             if friend_groups_set_num == 0:
@@ -294,12 +272,45 @@ class VkFriends():
             else:
                 self.root_friend_groups_set.difference_update(friend_groups_set)
 
-        print('Find exclusive groups completed: {} from {}'.format(counter, len(self.root_friend_id_set)))
+        print('Find exclusive groups completed: {} from {}'.format(counter, len(self.root_friends_id_set)))
         self.different_group_set = self.root_friend_groups_set
         print('Numbers of "tim_leary" exclusive groups: {}'.format(len(self.different_group_set)))
         logging.debug(u'Numbers of "tim_leary" exclusive groups: {}'.format(len(self.different_group_set)))
         logging.debug(u'different_group_set = {}'.format(self.root_friend_groups_set))
 
+    def make_root_friend_id_list(self):
+        """
+        https://api.vk.com/method/users.get?
+        user_id=<user_id> User, for which a list of friends is created
+         & access_token=<VK-token>
+         & v=<версия интерфейса VK>
+         & count=<количество выводимых id друзей>
+        :param self: <int> or <str> self.root_friend
+        :return: <int> friend_count and <list> friend_id_list
+        """
+        params = self.make_vk_params()
+        if SHORT_RUN:
+            params['count'] = 117
+        execution_code, response_json = self._do_vk_friends_get_request(params=params)
+        logging.info('make_root_friend_id_list: execution_code -> {}'.format(execution_code))
+        logging.debug('response_json -> {}'.format(response_json))
+
+        if execution_code:
+            self.root_friends_count = response_json['count']
+            self.root_friends_id_list = response_json['items']
+            self.root_friends_id_set = set(self.root_friends_id_list)
+
+    def _do_vk_friends_get_request(self, params):
+        execution_code, response_json = self._do_vk_request('friends.get', params)
+        logging.info('_do_vk_friends_get_request: execution_code -> {}'.format(execution_code))
+        logging.debug('response_json -> {}'.format(response_json))
+        try:
+            response_json = response_json['response']
+        except KeyError:
+            response_json = response_json['error']
+            execution_code = 0
+        finally:
+            return execution_code, response_json
 
     def _do_vk_request(self, method, params):
         """
@@ -311,11 +322,9 @@ class VkFriends():
         """
         any_vk_metods = ['users.get', 'friends.get', 'groups.get', 'groups.getMembers']
         logging.info('_do_vk_request() -> {}'.format(method))
-
         if not method in any_vk_metods:
             print('You try to call incorrect request "{}"'.format(method))
             return 0
-
 
         sleep(0.335)
         # response = requests.get('https://api.vk.com/method/users.get', params=params)
@@ -323,6 +332,7 @@ class VkFriends():
         if response.status_code == requests.codes.ok:
             response_json = response.json()
             request_have_done = True
+
             return 1, response_json
         else:
             logging.info('response')
@@ -338,17 +348,16 @@ class VkFriends():
 
 
 def main():
-    tim_leary_id = 5030613
-    tim_leary = VkFriends(root_vk_id) #  root_vk_id = '5030613'
-    # tim_leary = VkFriends(50032764534200241237464765874659873465347856)
+    # root_vk_id = 50032764534200241237464765874659873465347856  # test ID
+    tim_leary = VkFriends(root_vk_id) #  root_vk_id = '5030613' is tim_leary
 
     tim_leary.make_root_friend_id_list()
 
     tim_leary.root_friend_make_groups_set()
-    # tim_leary.print_root_user_info()
+    tim_leary.print_root_user_info()
     tim_leary.make_different_group_list()
 
-    # tim_leary.make_report_to_file()
+    tim_leary.make_report_to_file()
 
 
 if __name__ == '__main__':
